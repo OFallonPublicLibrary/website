e5c1227578c5a44815a94b366057bf5abe663c48
Kevin Dice <kevin.dice1@gmail.com>
2017-01-01 20:59:15 -0800

    Highlight current page

--- web/ofpl/search/views.py
+++ web/ofpl/search/views.py
@@ -37,4 +37,5 @@ def search(request):
         'search_query': search_query,
         'search_results': search_results,
         'page_number_range': page_number_range,
+        'current_page': paginator.number,
     })
