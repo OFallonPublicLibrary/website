04c9409cde563f3522b68fb4d93316de4990f3da
Kevin Dice <kevin.dice1@gmail.com>
2017-01-08 15:08:06 -0600

    Inherit swingtime classes

---
+++ web/ofpl/calendar/models.py
@@ -0,0 +1,20 @@
+import swingtime
+
+class Note(swingtime.Note):
+    pass
+
+
+class EventType(swingtime.EventType):
+    pass
+
+
+class Event(swingtime.Event):
+    pass
+
+
+class OccurrenceManager(swingtime.OccurrenceManager):
+    pass
+
+
+class Occurrence(swingtime.Occurrence):
+    pass
