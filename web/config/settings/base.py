f6c3ea876096ed2d8fed46224731978224f8147c
Kevin Dice <kevin.dice1@gmail.com>
2017-01-08 15:34:13 -0600

    Add back swingtime

--- web/config/settings/base.py
+++ web/config/settings/base.py
@@ -55,6 +55,7 @@ STOCK_APPS = [
 
 OTHER_APPS = [
     'compressor',
+    'swingtime',
 ]
 
 SOCIAL_APPS = [
@@ -89,6 +90,7 @@ PROJECT_APPS = [
     'ofpl.core',
     'ofpl.cms',
     'ofpl.search',
+    'ofpl.calendar',
 ]
 
 INSTALLED_APPS = STOCK_APPS + OTHER_APPS + SOCIAL_APPS + WAGTAIL_APPS + PROJECT_APPS
