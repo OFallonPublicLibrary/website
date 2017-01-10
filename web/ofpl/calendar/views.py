36f94f16cea060bf7e6fde225329c5198f7e3d1e
Kevin Dice <kevin.dice1@gmail.com>
2017-01-10 13:24:47 -0600

    Add calendar grid template

--- web/ofpl/calendar/views.py
+++ web/ofpl/calendar/views.py
@@ -6,8 +6,12 @@ from django.utils.encoding import smart_str
 from wagtail.wagtailadmin import messages
 from wagtail.wagtailcore.models import Page
 from .models import get_events_for_user
+from .permissions import permission_policy
 
 def index(request):
     return render(request, 'calendar/index.html', {
-
+        'user_can_add': permission_policy['occurrence'].user_has_permission(request.user, 'add'),
     })
+
+def add(request):
+    pass
