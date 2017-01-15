34ea273c175c2b6cac00e4d361b2ae6b517374c5
Kevin Dice <kevin.dice1@gmail.com>
2017-01-15 03:27:54 -0600

    More sensible subnav on depth 3 pages

--- web/ofpl/cms/templatetags/subnav_tags.py
+++ web/ofpl/cms/templatetags/subnav_tags.py
@@ -6,9 +6,18 @@ register = Library()
 
 @register.inclusion_tag('cms/subnav.html', takes_context=True)
 def subnav(context):
-    asdf = context['page'].get_ancestors()
-    return {
-        'heading': asdf[len(asdf)-1].title,
-        'page': context['page'],
-        'siblings': Page.objects.descendant_of(asdf[len(asdf)-1])
-    }
+    print(vars(context['page']))
+    page = context['page']
+    asdf = page.get_ancestors()
+    if page.depth == 4:
+        return {
+            'heading': asdf[len(asdf)-1].title,
+            'page': page,
+            'siblings': Page.objects.descendant_of(asdf[len(asdf)-1])
+        }
+    else:
+        return {
+            'heading': page.title,
+            'page': page,
+            'siblings': Page.objects.descendant_of(page)
+        }
